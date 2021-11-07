6e00a766ff7bc54e855493d3b81c9a6debe2af41
DoodleBob <riley.foust18@gmail.com>
2021-11-07 15:33:10 -0500

    added post route with unique ID

--- server.js
+++ server.js
@@ -10,11 +10,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-const notes = require("./db/db.json");
+const {notes} = require("./db/db.json");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+function createNewNote(body, notesArray) {
+  const newNote = body
+  notesArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({notes: notesArray}, null, 2)
+  );
+  // return finished code to the post route for response
+  return newNote;
+}
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
@@ -23,6 +30,17 @@ app.get("/notes", (req, res) => {
 app.get("/api/notes", (req, res) => {
   let results = notes;
   res.json(results);
+});
+
+app.post("/api/notes", (req, res) => {
+  req.body.id = uuidv4();
+
+  const newNote = createNewNote(req.body, notes);
+  res.json(newNote);
+})
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
